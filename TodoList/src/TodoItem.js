0afa243e22c9b82ae4d7c05ff16f0b9cc8ca6394
Yue Yu <yuyue730@gmail.com>
2019-03-23 16:47:07 -0400

    LifeCycle functions

---
+++ TodoList/src/TodoItem.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class TodoItem extends Component {
+
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  componentWillReceiveProps() {
+    console.log("TodoItem Calling componentWillReceiveProps");
+  }
+
+  componentWillUnmount() {
+    console.log("TodoItem Calling componentWillUnmount")
+  }
+
+  render() {
+    console.log("Child calling render")
+    const {test, content} = this.props;
+    return (
+      <div onClick={this.handleClick}>
+        {test} - {content}
+      </div>
+    );
+  }
+
+  handleClick() {
+    const { deleteItem, index } = this.props;
+    deleteItem(index);
+  }
+}
+
+TodoItem.propTypes = {
+  test: PropTypes.string.isRequired,
+  content: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  deleteItem: PropTypes.func,
+  index: PropTypes.number
+}
+
+TodoItem.defaultProps = {
+  test: 'Hello World'
+}
+
+export default TodoItem;
--- TodoList/src/TodoList.js
+++ TodoList/src/TodoList.js
@@ -1,59 +1,97 @@
 import React, {Component, Fragment} from 'react';
+import TodoItem from './TodoItem'
+import "./style.css"
 
 class TodoList extends Component {
 
   constructor(props) {
     super(props);
+    // Whenever, this.state or this.props is changed, render() will be executed again.
     this.state = {
-      inputValue: 'hello!!!',
+      inputValue: '',
       list: []
     }
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+    this.handleItemDelete = this.handleItemDelete.bind(this);
+  }
+
+  componentWillMount() {
+    console.log("Calling componentWillMount");
   }
 
   render() {
+    console.log("Parent calling render")
     return (
       <Fragment>
         <div>
+          {/*Input entry box and Submit button*/}
+          <label htmlFor='insertArea'>Please enter the item</label>
           <input 
-            value={this.state.inputValue}
-            onChange={this.handleInputChange.bind(this)}
+            id = 'insertArea'
+            value = {this.state.inputValue}
+            onChange = {this.handleInputChange}
+            className = 'input'
+            ref = {(input) => {this.input = input}}
           />
-          <button onClick={this.handButtonClick.bind(this)}>Submit</button>
+          <button onClick={this.handleButtonClick}>Submit</button>
         </div>
-        <ul>
-          {
-            this.state.list.map((item, index) => {
-              return (
-                <li key={index} onClick={this.handleItemDelete.bind(this, index)}>
-                  {item}
-                </li>
-              );
-            })
-          }
+        <ul ref={(ul) => {this.ul = ul}}>
+          {this.getTodoItem()}
         </ul>
       </Fragment>
     );
   }
 
-  handleInputChange(e) {
-    this.setState({
-      inputValue: e.target.value
-    });
+  componentDidMount() {
+    console.log("Calling componentDidMount");
   }
 
-  handButtonClick() {
-    this.setState({
-      list: [...this.state.list, this.state.inputValue],
+  shouldComponentUpdate() {
+    console.log("Calling shouldComponentUpdate");
+    return true;
+  }
+
+  componentWillUpdate() {
+    console.log("Calling componentWillUpdate");
+  }
+
+  componentDidUpdate() {
+    console.log("Calling componentDidUpdate");
+  }
+
+  getTodoItem() {
+    return this.state.list.map((item, index) => {
+        return (
+          <TodoItem 
+            key={index}
+            content={item} 
+            index={index}
+            deleteItem={this.handleItemDelete.bind(this)}
+          />
+        );
+      })
+  }
+
+  handleInputChange() {
+    const value = this.input.value;
+    this.setState(() => ({inputValue: value}));
+  }
+
+  handleButtonClick() {
+    this.setState((prevState) => ({
+      list: [...prevState.list, prevState.inputValue],
       inputValue: ''
-    })
+    }), () => {
+      console.log(this.ul.querySelectorAll('div').length);
+    });   // setState is asynchronized, so we need to the following work in the callback function
   }
 
   handleItemDelete(index) {
-    const list = [...this.state.list];
-    list.splice(index, 1);
-
-    this.setState({
-      list: list
+    this.setState((prevState) => {
+      const list = [...prevState.list];
+      list.splice(index, 1);
+      return {list}
     });
   }
 }
