09551fad9e471d35155851c8ecdb9c3bcdc4020a
Yue Yu <yuyue730@gmail.com>
2019-05-11 15:28:42 -0400

    Divide TodoList into UI component and Container component

--- todo_list_redux/src/TodoList.js
+++ todo_list_redux/src/TodoList.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import 'antd/dist/antd.css'
-import { Input, Button, List } from 'antd';
 import store from './store'
-import { getInputChangeAction, getAddItemAction, getDeleteItemAction } from './store/actionCreators'
+import { 
+  getInputChangeAction, getAddItemAction, getDeleteItemAction 
+} from './store/actionCreators'
+import TodoListUI from './TodoListUI'
 
 class TodoList extends Component {
-
   constructor(props) {
     super(props);
     this.state = store.getState();
@@ -18,27 +19,13 @@ class TodoList extends Component {
 
   render() {
     return (
-      <div style={{marginTop: '10px', marginLeft: '10px'}}>
-        <div>
-          <Input 
-            value={this.state.inputValue} 
-            placeholder='Todo Info' 
-            style={{width: '300px', marginRight: '10px'}}
-            onChange={this.handleInputChange}
-          />
-          <Button type="primary" onClick={this.handleButtonClick}>Submit</Button>
-        </div>
-        <List
-          style={{marginTop: '10px', width: '300px'}}
-          bordered
-          dataSource={this.state.list}
-          renderItem={(item, index) => (
-            <List.Item onClick={this.handleItemDelete.bind(this, index)}>
-              {item}
-            </List.Item>
-          )}
-        />
-      </div>
+      <TodoListUI 
+        inputValue={this.state.inputValue}
+        list={this.state.list}
+        handleInputChange={this.handleInputChange}
+        handleButtonClick={this.handleButtonClick}
+        handleItemDelete={this.handleItemDelete}
+      />
     );
   }
 
