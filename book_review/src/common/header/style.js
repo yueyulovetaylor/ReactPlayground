02ac1b7552875329e4750a60019c4687b887e01a
Yue Yu <yuyue730@gmail.com>
2019-05-12 18:39:43 -0400

    Add CSSTransition

--- book_review/src/common/header/style.js
+++ book_review/src/common/header/style.js
@@ -47,6 +47,18 @@ export const NavItem = styled.div`
 export const SearchWrapper = styled.div`
   position: relative
   float: left;
+  .slide-enter {
+    transition: all .2s ease-out;
+  }
+  .slide-enter-active {
+    width: 240px;
+  }
+  .slide-exit {
+    transition: all .2s ease-out;
+  }
+  .slide-exit-active {
+    width: 160px;
+  }
   .iconfont {
     position: absolute;
     right: 5px;
@@ -55,6 +67,10 @@ export const SearchWrapper = styled.div`
     line-height: 30px;
     text-align: center;
     border-radius: 15px;
+    &.focused {
+      background: #777;
+      color: #fff
+    }
   }
 `;
 
@@ -65,15 +81,19 @@ export const NavSearch = styled.input.attrs({
   height: 38px;
   margin-top: 9px;
   margin-left: 20px;
-  padding: 0 20px;
+  padding: 0 30px 0 20px;
   box-sizing: border-box;
   border: none;
   outline: none;
   border-radius: 19px;
   background: #eee;
   font-size: 14px;
+  color: #666
   &::placeholder {
     color: #999;
+  };
+  &.focused {
+    width: 240px;
   };
 `
 
